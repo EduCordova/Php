d0c20e8f25a948cd61b5003db7b31f69d5796d2d
Eduardo <kde1690@gmail.com>
2017-11-01 21:34:23 -0500

    Create PhpWithCsv.php

---
+++ PhpWithCsv.php
@@ -0,0 +1,22 @@
+require "dom_php.php";
+
+$html = new simple_html_dom();
+
+$html -> load_file("http://www.genbetadev.com/");
+
+$tile = $html->find('h2 a');
+$csvArry = [];
+
+foreach ($tile as $ti){
+    $nti = $ti->innertext;
+    $csvArry[][]=$nti;
+}
+$filename = "dat.csv";
+
+$fp = fopen($filename,'w');
+
+fprintf( $fp, "\xEF\xBB\xBF");
+foreach ($csvArry as $datos){
+    fputcsv($fp,$datos);
+}
+fclose($fp);
